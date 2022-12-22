356e60945ddf8b27a19210702c6098844e6e76f8
huangtao <huangtao19900205@outlook.com>
2022-12-22 12:52:23 +0800

    optimize

--- compDev/index.jsx
+++ compDev/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import VirtualList from '../virtualList';
 
 document.addEventListener('DOMContentLoaded', function () {
   if (!window.helper.root) {
     window.helper.root = ReactDom.createRoot(document.getElementById('root'));
-    window.helper.root.render(<VirtualList />);
+    window.helper.root.render(<div>huangtao</div>);
   }
 });
